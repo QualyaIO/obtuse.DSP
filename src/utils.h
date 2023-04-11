937d9690ba9ce2092b0793cfe3d9b438c2dfdec3
jfrey <nomhad@gmail.com>
2023-04-11 03:33:22 +0200

    importing tonnetz

--- src/utils.h
+++ src/utils.h
@@ -7,6 +7,268 @@
 #include "vultin.h"
 #include "utils.tables.h"
 
+typedef struct utils__tuple___int_int_int__ {
+   int field_0;
+   int field_1;
+   int field_2;
+} utils__tuple___int_int_int__;
+
+static_inline fix16_t utils_Tonnetz_spow(fix16_t base, fix16_t x){
+   fix16_t log_base;
+   log_base = fix_mul(0x24d76 /* 2.302585 */,log10(base));
+   return fix_exp(fix_mul(log_base,x));
+}
+
+static_inline fix16_t utils_Tonnetz_beta(fix16_t x, fix16_t b){
+   return (0x10000 /* 1.000000 */ + (- utils_Tonnetz_spow((0x10000 /* 1.000000 */ + (- x)),b)));
+};
+
+void utils_Tonnetz_getScale(int id, uint8_t (&_output_)[12]);
+
+void utils_Tonnetz_getChord(int chord, int (&_output_)[3]);
+
+void utils_Tonnetz_getChordInversion(int chord, int inversion, int (&_output_)[3]);
+
+typedef struct utils_Tonnetz__ctx_type_4 {
+   int shifts[11];
+   int shift;
+   int scaleId;
+   uint8_t scale[12];
+   int root;
+   int process_ret_2;
+   int process_ret_1;
+   int process_ret_0;
+   fix16_t pJump;
+   fix16_t inversions[3];
+   fix16_t inversionSpread;
+   fix16_t chords[6];
+   fix16_t chordSpread;
+   int chord;
+} utils_Tonnetz__ctx_type_4;
+
+typedef utils_Tonnetz__ctx_type_4 utils_Tonnetz_isChordInShift_type;
+
+void utils_Tonnetz__ctx_type_4_init(utils_Tonnetz__ctx_type_4 &_output_);
+
+static_inline void utils_Tonnetz_isChordInShift_init(utils_Tonnetz__ctx_type_4 &_output_){
+   utils_Tonnetz__ctx_type_4_init(_output_);
+   return ;
+}
+
+uint8_t utils_Tonnetz_isChordInShift(utils_Tonnetz__ctx_type_4 &_ctx, int checkChord, int checkShift);
+
+typedef utils_Tonnetz__ctx_type_4 utils_Tonnetz_drawChord_type;
+
+static_inline void utils_Tonnetz_drawChord_init(utils_Tonnetz__ctx_type_4 &_output_){
+   utils_Tonnetz__ctx_type_4_init(_output_);
+   return ;
+}
+
+int utils_Tonnetz_drawChord(utils_Tonnetz__ctx_type_4 &_ctx, uint8_t lookAround);
+
+typedef utils_Tonnetz__ctx_type_4 utils_Tonnetz_drawInversion_type;
+
+static_inline void utils_Tonnetz_drawInversion_init(utils_Tonnetz__ctx_type_4 &_output_){
+   utils_Tonnetz__ctx_type_4_init(_output_);
+   return ;
+}
+
+int utils_Tonnetz_drawInversion(utils_Tonnetz__ctx_type_4 &_ctx);
+
+typedef utils_Tonnetz__ctx_type_4 utils_Tonnetz_process_type;
+
+static_inline void utils_Tonnetz_process_init(utils_Tonnetz__ctx_type_4 &_output_){
+   utils_Tonnetz__ctx_type_4_init(_output_);
+   return ;
+}
+
+void utils_Tonnetz_process(utils_Tonnetz__ctx_type_4 &_ctx);
+
+typedef utils_Tonnetz__ctx_type_4 utils_Tonnetz_process_ret_0_type;
+
+static_inline void utils_Tonnetz_process_ret_0_init(utils_Tonnetz__ctx_type_4 &_output_){
+   utils_Tonnetz__ctx_type_4_init(_output_);
+   return ;
+}
+
+static_inline int utils_Tonnetz_process_ret_0(utils_Tonnetz__ctx_type_4 &_ctx){
+   return _ctx.process_ret_0;
+};
+
+typedef utils_Tonnetz__ctx_type_4 utils_Tonnetz_process_ret_1_type;
+
+static_inline void utils_Tonnetz_process_ret_1_init(utils_Tonnetz__ctx_type_4 &_output_){
+   utils_Tonnetz__ctx_type_4_init(_output_);
+   return ;
+}
+
+static_inline int utils_Tonnetz_process_ret_1(utils_Tonnetz__ctx_type_4 &_ctx){
+   return _ctx.process_ret_1;
+};
+
+typedef utils_Tonnetz__ctx_type_4 utils_Tonnetz_process_ret_2_type;
+
+static_inline void utils_Tonnetz_process_ret_2_init(utils_Tonnetz__ctx_type_4 &_output_){
+   utils_Tonnetz__ctx_type_4_init(_output_);
+   return ;
+}
+
+static_inline int utils_Tonnetz_process_ret_2(utils_Tonnetz__ctx_type_4 &_ctx){
+   return _ctx.process_ret_2;
+};
+
+typedef utils_Tonnetz__ctx_type_4 utils_Tonnetz_reset_type;
+
+static_inline void utils_Tonnetz_reset_init(utils_Tonnetz__ctx_type_4 &_output_){
+   utils_Tonnetz__ctx_type_4_init(_output_);
+   return ;
+}
+
+void utils_Tonnetz_reset(utils_Tonnetz__ctx_type_4 &_ctx);
+
+typedef utils_Tonnetz__ctx_type_4 utils_Tonnetz_setScale_type;
+
+static_inline void utils_Tonnetz_setScale_init(utils_Tonnetz__ctx_type_4 &_output_){
+   utils_Tonnetz__ctx_type_4_init(_output_);
+   return ;
+}
+
+void utils_Tonnetz_setScale(utils_Tonnetz__ctx_type_4 &_ctx, int id);
+
+typedef utils_Tonnetz__ctx_type_4 utils_Tonnetz_setRoot_type;
+
+static_inline void utils_Tonnetz_setRoot_init(utils_Tonnetz__ctx_type_4 &_output_){
+   utils_Tonnetz__ctx_type_4_init(_output_);
+   return ;
+}
+
+static_inline void utils_Tonnetz_setRoot(utils_Tonnetz__ctx_type_4 &_ctx, int note){
+   note = int_clip(note,0,127);
+   if(_ctx.root != note){
+      _ctx.root = note;
+   }
+}
+
+typedef utils_Tonnetz__ctx_type_4 utils_Tonnetz__updateChords_type;
+
+static_inline void utils_Tonnetz__updateChords_init(utils_Tonnetz__ctx_type_4 &_output_){
+   utils_Tonnetz__ctx_type_4_init(_output_);
+   return ;
+}
+
+void utils_Tonnetz__updateChords(utils_Tonnetz__ctx_type_4 &_ctx, fix16_t bParam);
+
+typedef utils_Tonnetz__ctx_type_4 utils_Tonnetz__updateInversions_type;
+
+static_inline void utils_Tonnetz__updateInversions_init(utils_Tonnetz__ctx_type_4 &_output_){
+   utils_Tonnetz__ctx_type_4_init(_output_);
+   return ;
+}
+
+void utils_Tonnetz__updateInversions(utils_Tonnetz__ctx_type_4 &_ctx, fix16_t bParam);
+
+typedef utils_Tonnetz__ctx_type_4 utils_Tonnetz_setChord_type;
+
+static_inline void utils_Tonnetz_setChord_init(utils_Tonnetz__ctx_type_4 &_output_){
+   utils_Tonnetz__ctx_type_4_init(_output_);
+   return ;
+}
+
+static_inline void utils_Tonnetz_setChord(utils_Tonnetz__ctx_type_4 &_ctx, int newChord){
+   _ctx.chord = int_clip(newChord,0,5);
+};
+
+typedef utils_Tonnetz__ctx_type_4 utils_Tonnetz_setChordSpread_type;
+
+static_inline void utils_Tonnetz_setChordSpread_init(utils_Tonnetz__ctx_type_4 &_output_){
+   utils_Tonnetz__ctx_type_4_init(_output_);
+   return ;
+}
+
+void utils_Tonnetz_setChordSpread(utils_Tonnetz__ctx_type_4 &_ctx, fix16_t newSpread);
+
+typedef utils_Tonnetz__ctx_type_4 utils_Tonnetz_setInversionSpread_type;
+
+static_inline void utils_Tonnetz_setInversionSpread_init(utils_Tonnetz__ctx_type_4 &_output_){
+   utils_Tonnetz__ctx_type_4_init(_output_);
+   return ;
+}
+
+void utils_Tonnetz_setInversionSpread(utils_Tonnetz__ctx_type_4 &_ctx, fix16_t newSpread);
+
+typedef utils_Tonnetz__ctx_type_4 utils_Tonnetz_getChordP_type;
+
+static_inline void utils_Tonnetz_getChordP_init(utils_Tonnetz__ctx_type_4 &_output_){
+   utils_Tonnetz__ctx_type_4_init(_output_);
+   return ;
+}
+
+fix16_t utils_Tonnetz_getChordP(utils_Tonnetz__ctx_type_4 &_ctx, int chordId);
+
+typedef utils_Tonnetz__ctx_type_4 utils_Tonnetz_getNbChords_type;
+
+static_inline void utils_Tonnetz_getNbChords_init(utils_Tonnetz__ctx_type_4 &_output_){
+   utils_Tonnetz__ctx_type_4_init(_output_);
+   return ;
+}
+
+static_inline int utils_Tonnetz_getNbChords(utils_Tonnetz__ctx_type_4 &_ctx){
+   return 6;
+};
+
+typedef utils_Tonnetz__ctx_type_4 utils_Tonnetz_getScaleId_type;
+
+static_inline void utils_Tonnetz_getScaleId_init(utils_Tonnetz__ctx_type_4 &_output_){
+   utils_Tonnetz__ctx_type_4_init(_output_);
+   return ;
+}
+
+static_inline int utils_Tonnetz_getScaleId(utils_Tonnetz__ctx_type_4 &_ctx){
+   return _ctx.scaleId;
+};
+
+typedef utils_Tonnetz__ctx_type_4 utils_Tonnetz_getNbScales_type;
+
+static_inline void utils_Tonnetz_getNbScales_init(utils_Tonnetz__ctx_type_4 &_output_){
+   utils_Tonnetz__ctx_type_4_init(_output_);
+   return ;
+}
+
+static_inline int utils_Tonnetz_getNbScales(utils_Tonnetz__ctx_type_4 &_ctx){
+   return 20;
+};
+
+typedef utils_Tonnetz__ctx_type_4 utils_Tonnetz_getRoot_type;
+
+static_inline void utils_Tonnetz_getRoot_init(utils_Tonnetz__ctx_type_4 &_output_){
+   utils_Tonnetz__ctx_type_4_init(_output_);
+   return ;
+}
+
+static_inline int utils_Tonnetz_getRoot(utils_Tonnetz__ctx_type_4 &_ctx){
+   return _ctx.root;
+};
+
+typedef utils_Tonnetz__ctx_type_4 utils_Tonnetz_setJump_type;
+
+static_inline void utils_Tonnetz_setJump_init(utils_Tonnetz__ctx_type_4 &_output_){
+   utils_Tonnetz__ctx_type_4_init(_output_);
+   return ;
+}
+
+static_inline void utils_Tonnetz_setJump(utils_Tonnetz__ctx_type_4 &_ctx, fix16_t p){
+   _ctx.pJump = fix_clip(p,0x0 /* 0.000000 */,0x10000 /* 1.000000 */);
+};
+
+typedef utils_Tonnetz__ctx_type_4 utils_Tonnetz_default_type;
+
+static_inline void utils_Tonnetz_default_init(utils_Tonnetz__ctx_type_4 &_output_){
+   utils_Tonnetz__ctx_type_4_init(_output_);
+   return ;
+}
+
+void utils_Tonnetz_default(utils_Tonnetz__ctx_type_4 &_ctx);
+
 typedef struct utils_Trigg__ctx_type_0 {
    uint8_t triggers[128];
    int ticks;
@@ -213,13 +475,17 @@ static_inline fix16_t utils_Clock_getMaxBPM(){
    return 0x75300000 /* 30000.000000 */;
 };
 
-typedef struct utils_Clock__ctx_type_4 {
+int utils_Clock_compareTimeFract(int time1S, fix16_t time1Fract, int time2S, fix16_t time2Fract);
+
+typedef struct utils_Clock__ctx_type_5 {
    fix16_t swing;
    int subSize;
    int pos;
    uint8_t orderMix;
-   fix16_t lastTime;
-   fix16_t lastBeat;
+   int lastTimeS;
+   fix16_t lastTimeFract;
+   int lastBeatS;
+   fix16_t lastBeatFract;
    uint8_t init;
    fix16_t ibiB;
    fix16_t ibiA;
@@ -227,94 +493,94 @@ typedef struct utils_Clock__ctx_type_4 {
    int groupSize;
    fix16_t groupRatio;
    fix16_t bpm;
-} utils_Clock__ctx_type_4;
+} utils_Clock__ctx_type_5;
 
-typedef utils_Clock__ctx_type_4 utils_Clock_process_type;
+typedef utils_Clock__ctx_type_5 utils_Clock_process_type;
 
-void utils_Clock__ctx_type_4_init(utils_Clock__ctx_type_4 &_output_);
+void utils_Clock__ctx_type_5_init(utils_Clock__ctx_type_5 &_output_);
 
-static_inline void utils_Clock_process_init(utils_Clock__ctx_type_4 &_output_){
-   utils_Clock__ctx_type_4_init(_output_);
+static_inline void utils_Clock_process_init(utils_Clock__ctx_type_5 &_output_){
+   utils_Clock__ctx_type_5_init(_output_);
    return ;
 }
 
-int utils_Clock_process(utils_Clock__ctx_type_4 &_ctx, fix16_t time);
+int utils_Clock_process(utils_Clock__ctx_type_5 &_ctx, int timeS, fix16_t timeFract);
 
-typedef utils_Clock__ctx_type_4 utils_Clock_reset_type;
+typedef utils_Clock__ctx_type_5 utils_Clock_reset_type;
 
-static_inline void utils_Clock_reset_init(utils_Clock__ctx_type_4 &_output_){
-   utils_Clock__ctx_type_4_init(_output_);
+static_inline void utils_Clock_reset_init(utils_Clock__ctx_type_5 &_output_){
+   utils_Clock__ctx_type_5_init(_output_);
    return ;
 }
 
-static_inline void utils_Clock_reset(utils_Clock__ctx_type_4 &_ctx){
+static_inline void utils_Clock_reset(utils_Clock__ctx_type_5 &_ctx){
    _ctx.init = false;
 };
 
-typedef utils_Clock__ctx_type_4 utils_Clock__recompute_type;
+typedef utils_Clock__ctx_type_5 utils_Clock__recompute_type;
 
-static_inline void utils_Clock__recompute_init(utils_Clock__ctx_type_4 &_output_){
-   utils_Clock__ctx_type_4_init(_output_);
+static_inline void utils_Clock__recompute_init(utils_Clock__ctx_type_5 &_output_){
+   utils_Clock__ctx_type_5_init(_output_);
    return ;
 }
 
-void utils_Clock__recompute(utils_Clock__ctx_type_4 &_ctx);
+void utils_Clock__recompute(utils_Clock__ctx_type_5 &_ctx);
 
-typedef utils_Clock__ctx_type_4 utils_Clock_setBPM_type;
+typedef utils_Clock__ctx_type_5 utils_Clock_setBPM_type;
 
-static_inline void utils_Clock_setBPM_init(utils_Clock__ctx_type_4 &_output_){
-   utils_Clock__ctx_type_4_init(_output_);
+static_inline void utils_Clock_setBPM_init(utils_Clock__ctx_type_5 &_output_){
+   utils_Clock__ctx_type_5_init(_output_);
    return ;
 }
 
-void utils_Clock_setBPM(utils_Clock__ctx_type_4 &_ctx, fix16_t newBPM);
+void utils_Clock_setBPM(utils_Clock__ctx_type_5 &_ctx, fix16_t newBPM);
 
-typedef utils_Clock__ctx_type_4 utils_Clock_setGroupSize_type;
+typedef utils_Clock__ctx_type_5 utils_Clock_setGroupSize_type;
 
-static_inline void utils_Clock_setGroupSize_init(utils_Clock__ctx_type_4 &_output_){
-   utils_Clock__ctx_type_4_init(_output_);
+static_inline void utils_Clock_setGroupSize_init(utils_Clock__ctx_type_5 &_output_){
+   utils_Clock__ctx_type_5_init(_output_);
    return ;
 }
 
-void utils_Clock_setGroupSize(utils_Clock__ctx_type_4 &_ctx, int newGroupSize);
+void utils_Clock_setGroupSize(utils_Clock__ctx_type_5 &_ctx, int newGroupSize);
 
-typedef utils_Clock__ctx_type_4 utils_Clock_setGroupRatio_type;
+typedef utils_Clock__ctx_type_5 utils_Clock_setGroupRatio_type;
 
-static_inline void utils_Clock_setGroupRatio_init(utils_Clock__ctx_type_4 &_output_){
-   utils_Clock__ctx_type_4_init(_output_);
+static_inline void utils_Clock_setGroupRatio_init(utils_Clock__ctx_type_5 &_output_){
+   utils_Clock__ctx_type_5_init(_output_);
    return ;
 }
 
-void utils_Clock_setGroupRatio(utils_Clock__ctx_type_4 &_ctx, fix16_t newGroupRatio);
+void utils_Clock_setGroupRatio(utils_Clock__ctx_type_5 &_ctx, fix16_t newGroupRatio);
 
-typedef utils_Clock__ctx_type_4 utils_Clock_setSwing_type;
+typedef utils_Clock__ctx_type_5 utils_Clock_setSwing_type;
 
-static_inline void utils_Clock_setSwing_init(utils_Clock__ctx_type_4 &_output_){
-   utils_Clock__ctx_type_4_init(_output_);
+static_inline void utils_Clock_setSwing_init(utils_Clock__ctx_type_5 &_output_){
+   utils_Clock__ctx_type_5_init(_output_);
    return ;
 }
 
-void utils_Clock_setSwing(utils_Clock__ctx_type_4 &_ctx, fix16_t newSwing);
+void utils_Clock_setSwing(utils_Clock__ctx_type_5 &_ctx, fix16_t newSwing);
 
-typedef utils_Clock__ctx_type_4 utils_Clock_setOrderMix_type;
+typedef utils_Clock__ctx_type_5 utils_Clock_setOrderMix_type;
 
-static_inline void utils_Clock_setOrderMix_init(utils_Clock__ctx_type_4 &_output_){
-   utils_Clock__ctx_type_4_init(_output_);
+static_inline void utils_Clock_setOrderMix_init(utils_Clock__ctx_type_5 &_output_){
+   utils_Clock__ctx_type_5_init(_output_);
    return ;
 }
 
-static_inline void utils_Clock_setOrderMix(utils_Clock__ctx_type_4 &_ctx, uint8_t flag){
+static_inline void utils_Clock_setOrderMix(utils_Clock__ctx_type_5 &_ctx, uint8_t flag){
    _ctx.orderMix = flag;
 };
 
-typedef utils_Clock__ctx_type_4 utils_Clock_default_type;
+typedef utils_Clock__ctx_type_5 utils_Clock_default_type;
 
-static_inline void utils_Clock_default_init(utils_Clock__ctx_type_4 &_output_){
-   utils_Clock__ctx_type_4_init(_output_);
+static_inline void utils_Clock_default_init(utils_Clock__ctx_type_5 &_output_){
+   utils_Clock__ctx_type_5_init(_output_);
    return ;
 }
 
-static_inline void utils_Clock_default(utils_Clock__ctx_type_4 &_ctx){
+static_inline void utils_Clock_default(utils_Clock__ctx_type_5 &_ctx){
    utils_Clock_setBPM(_ctx,0x780000 /* 120.000000 */);
    utils_Clock_setGroupSize(_ctx,4);
    utils_Clock_setGroupRatio(_ctx,0x8000 /* 0.500000 */);
