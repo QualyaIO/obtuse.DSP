0a3603e137b3f7d821752b1786cd99877c3acd67
jfrey <nomhad@gmail.com>
2023-04-24 01:04:30 +0200

    sampler: adding wrapper

--- src/synthSampler.cpp
+++ src/synthSampler.cpp
@@ -91,8 +91,8 @@ uint8_t synthSampler_Notes_noteOff(synthSampler_Notes__ctx_type_0 &_ctx, int not
    return false;
 }
 
-void synthSampler_Sampler__ctx_type_2_init(synthSampler_Sampler__ctx_type_2 &_output_){
-   synthSampler_Sampler__ctx_type_2 &_ctx = _output_;
+void synthSampler_Sampler__ctx_type_0_init(synthSampler_Sampler__ctx_type_0 &_output_){
+   synthSampler_Sampler__ctx_type_0 &_ctx = _output_;
    _ctx.step = 0x0 /* 0.000000 */;
    _ctx.state = 0;
    _ctx.size = 0;
@@ -117,7 +117,7 @@ void synthSampler_Sampler__ctx_type_2_init(synthSampler_Sampler__ctx_type_2 &_ou
    return ;
 }
 
-fix16_t synthSampler_Sampler_process(synthSampler_Sampler__ctx_type_2 &_ctx){
+fix16_t synthSampler_Sampler_process(synthSampler_Sampler__ctx_type_0 &_ctx){
    fix16_t value;
    value = 0x0 /* 0.000000 */;
    if(_ctx.state > 0){
@@ -166,14 +166,14 @@ fix16_t synthSampler_Sampler_process(synthSampler_Sampler__ctx_type_2 &_ctx){
          }
          else
          {
-            value = fix_mul(_ctx.level,(synthSampler_Sampler_getSample(_ctx,idx) + fix_mul((_ctx.pos % 0x10000 /* 1.000000 */),(synthSampler_Sampler_getSample(_ctx,(1 + idx)) + (- synthSampler_Sampler_getSample(_ctx,idx))))));
+            value = fix_mul(_ctx.level,(synthSampler_SampleWrapper_getSample(idx) + fix_mul((_ctx.pos % 0x10000 /* 1.000000 */),(synthSampler_SampleWrapper_getSample((1 + idx)) + (- synthSampler_SampleWrapper_getSample(idx))))));
          }
       }
    }
    return value;
 }
 
-void synthSampler_Sampler_process_bufferTo(synthSampler_Sampler__ctx_type_2 &_ctx, int nb, fix16_t (&oBuffer)[256]){
+void synthSampler_Sampler_process_bufferTo(synthSampler_Sampler__ctx_type_0 &_ctx, int nb, fix16_t (&oBuffer)[256]){
    nb = int_clip(nb,0,256);
    if(nb == 0){
       nb = 256;
@@ -229,7 +229,7 @@ void synthSampler_Sampler_process_bufferTo(synthSampler_Sampler__ctx_type_2 &_ct
             }
             else
             {
-               oBuffer[i] = fix_mul(_ctx.level,(synthSampler_Sampler_getSample(_ctx,idx) + fix_mul((_ctx.pos % 0x10000 /* 1.000000 */),(synthSampler_Sampler_getSample(_ctx,(1 + idx)) + (- synthSampler_Sampler_getSample(_ctx,idx))))));
+               oBuffer[i] = fix_mul(_ctx.level,(synthSampler_SampleWrapper_getSample(idx) + fix_mul((_ctx.pos % 0x10000 /* 1.000000 */),(synthSampler_SampleWrapper_getSample((1 + idx)) + (- synthSampler_SampleWrapper_getSample(idx))))));
             }
          }
       }
@@ -241,7 +241,7 @@ void synthSampler_Sampler_process_bufferTo(synthSampler_Sampler__ctx_type_2 &_ct
    }
 }
 
-void synthSampler_Sampler_setSamplerate(synthSampler_Sampler__ctx_type_2 &_ctx, fix16_t newFs){
+void synthSampler_Sampler_setSamplerate(synthSampler_Sampler__ctx_type_0 &_ctx, fix16_t newFs){
    if(newFs > 0x0 /* 0.000000 */){
       _ctx.fs = newFs;
       _ctx.fsRatio = fix_div(_ctx.sampleFs,_ctx.fs);
@@ -249,7 +249,7 @@ void synthSampler_Sampler_setSamplerate(synthSampler_Sampler__ctx_type_2 &_ctx, 
    synthSampler_Sampler_updateStep(_ctx);
 }
 
-void synthSampler_Sampler_updateCrossFade(synthSampler_Sampler__ctx_type_2 &_ctx){
+void synthSampler_Sampler_updateCrossFade(synthSampler_Sampler__ctx_type_0 &_ctx){
    if((_ctx.loopS > (256 / 2)) && ((_ctx.size + (- _ctx.loopE)) > (1 + (256 / 2)))){
       _ctx.crossfade = true;
       int i;
@@ -262,7 +262,7 @@ void synthSampler_Sampler_updateCrossFade(synthSampler_Sampler__ctx_type_2 &_ctx
       idxE = (_ctx.loopE + (- (256 / 2)));
       while(i < 256){
          k = fix_mul(0x101 /* 0.003922 */,int_to_fix(i));
-         _ctx.buffer_cross[i] = (fix_mul(k,synthSampler_Sampler_getSample(_ctx,(i + idxS))) + fix_mul(synthSampler_Sampler_getSample(_ctx,(i + idxE)),(0x10000 /* 1.000000 */ + (- k))));
+         _ctx.buffer_cross[i] = (fix_mul(k,synthSampler_SampleWrapper_getSample((i + idxS))) + fix_mul(synthSampler_SampleWrapper_getSample((i + idxE)),(0x10000 /* 1.000000 */ + (- k))));
          i = (1 + i);
       }
    }
@@ -272,7 +272,7 @@ void synthSampler_Sampler_updateCrossFade(synthSampler_Sampler__ctx_type_2 &_ctx
    }
 }
 
-void synthSampler_Sampler_setNote(synthSampler_Sampler__ctx_type_2 &_ctx, int note){
+void synthSampler_Sampler_setNote(synthSampler_Sampler__ctx_type_0 &_ctx, int note){
    fix16_t log_two;
    log_two = 0xb172 /* 0.693147 */;
    fix16_t semitones;
@@ -281,7 +281,7 @@ void synthSampler_Sampler_setNote(synthSampler_Sampler__ctx_type_2 &_ctx, int no
    synthSampler_Sampler_updateStep(_ctx);
 }
 
-void synthSampler_Sampler_noteOn(synthSampler_Sampler__ctx_type_2 &_ctx, int note, int velocity, int channel){
+void synthSampler_Sampler_noteOn(synthSampler_Sampler__ctx_type_0 &_ctx, int note, int velocity, int channel){
    note = int_clip(note,0,127);
    if(synthSampler_Notes_noteOn(_ctx.playingnotes,note,velocity,channel)){
       synthSampler_Sampler_setNote(_ctx,note);
@@ -293,7 +293,7 @@ void synthSampler_Sampler_noteOn(synthSampler_Sampler__ctx_type_2 &_ctx, int not
    }
 }
 
-void synthSampler_Sampler_noteOff(synthSampler_Sampler__ctx_type_2 &_ctx, int note, int channel){
+void synthSampler_Sampler_noteOff(synthSampler_Sampler__ctx_type_0 &_ctx, int note, int channel){
    note = int_clip(note,0,127);
    if(synthSampler_Notes_noteOff(_ctx.playingnotes,note,channel)){
       if(synthSampler_Notes_nbNotes(_ctx.playingnotes) > 0){
@@ -310,15 +310,23 @@ void synthSampler_Sampler_noteOff(synthSampler_Sampler__ctx_type_2 &_ctx, int no
    }
 }
 
-void synthSampler_Sampler_default(synthSampler_Sampler__ctx_type_2 &_ctx){
-   _ctx.size = synthSampler_Sampler_ocarina_samples();
+void synthSampler_Sampler_default(synthSampler_Sampler__ctx_type_0 &_ctx){
+   _ctx.size = synthSampler_SampleWrapper_getSampleSize();
    synthSampler_Buffer_buffer(_ctx.buffer_cross);
    _ctx.crossfade = false;
    _ctx.sampleFs = 0x1e0000 /* 30.000000 */;
    _ctx.sampleNote = 72;
-   synthSampler_Sampler_setLoop(_ctx,true);
-   synthSampler_Sampler_setLoopStart(_ctx,5073);
-   synthSampler_Sampler_setLoopEnd(_ctx,5992);
+   if(true){
+      int lS;
+      lS = 5073;
+      int lE;
+      lE = 5992;
+      if((lS >= 0) && (lE < _ctx.size) && (lE >= lS)){
+         synthSampler_Sampler_setLoop(_ctx,true);
+         synthSampler_Sampler_setLoopStart(_ctx,lS);
+         synthSampler_Sampler_setLoopEnd(_ctx,lE);
+      }
+   }
    synthSampler_Buffer_buffer(_ctx.buffer_o);
    synthSampler_Sampler_setLevel(_ctx,0x10000 /* 1.000000 */);
    synthSampler_Sampler_setSamplerate(_ctx,0x2c1999 /* 44.100000 */);
@@ -329,10 +337,10 @@ void synthSampler_Sampler_default(synthSampler_Sampler__ctx_type_2 &_ctx){
 
 void synthSampler_Poly__ctx_type_0_init(synthSampler_Poly__ctx_type_0 &_output_){
    synthSampler_Poly__ctx_type_0 &_ctx = _output_;
-   synthSampler_Sampler__ctx_type_2_init(_ctx.voice3);
-   synthSampler_Sampler__ctx_type_2_init(_ctx.voice2);
-   synthSampler_Sampler__ctx_type_2_init(_ctx.voice1);
-   synthSampler_Sampler__ctx_type_2_init(_ctx.voice0);
+   synthSampler_Sampler__ctx_type_0_init(_ctx.voice3);
+   synthSampler_Sampler__ctx_type_0_init(_ctx.voice2);
+   synthSampler_Sampler__ctx_type_0_init(_ctx.voice1);
+   synthSampler_Sampler__ctx_type_0_init(_ctx.voice0);
    _ctx.should_leftovers = false;
    synthSampler_Poly_default(_ctx);
    
