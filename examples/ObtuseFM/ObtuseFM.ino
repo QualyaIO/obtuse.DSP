907a9a4b9f5d5f5c248ffacaf8d8921807c8403c
jfrey <nomhad@gmail.com>
2025-02-13 16:50:49 +0100

    worknig on simpler version for tentative UNO

---
+++ examples/ObtuseFM/ObtuseFM.ino
@@ -0,0 +1,95 @@
+
+// This example is meant to run on a Uno with PWM output
+// Tested Arduino IDE 1.8.19 with obtuse 0.2.0
+
+
+/*** Obtuse DSP ***/
+
+#include "synthFMalt.h"
+
+
+// contexts in obtuse, used to handle internal states
+synthFMalt_OSCalt_process_type contextv0;
+
+// we will be using the buffered processes for each synth
+// to sync with obtuse's vult code
+#define BUFFER_SIZE 128
+
+// output buf
+int16_t buff[BUFFER_SIZE];
+// voices
+fix16_t raw_buff[BUFFER_SIZE];
+
+/*** Audio output ***/
+
+// 30khz audio, tradeoff between quality and CPU load
+const int sampleRate =  30000;
+
+/*** Misc ***/
+
+// counter for debug
+int tick = 0;
+
+// computing time spent on DSP
+unsigned long dsp_tick;
+unsigned long dsp_time;
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+  Serial.println("Let us go");
+
+  /* Audio */
+
+  /* Obtuse DSP */
+  // Init FM, then pass sample rate, not forgetting to convert passed parameters to fixed (of course...)
+  synthFMalt_OSCalt_default(contextv0);
+  synthFMalt_OSCalt_setSamplerate(contextv0, float_to_fix(sampleRate / (float)1000));
+  // speed-up to get rid of normalization across multiple voices, might saturate signal though
+  //synthFM_Voice_setNormalize(contextv0, false);
+}
+
+void loop() {
+  // buffers hard-coded of size 16 (unless setBuffers() is called), with mono output we need as much as one buffer available
+  // Make sure there are at least two samples above that free in the audio circular buffer (of buffers)
+  // Note: the returned value of availableForWrite() changed across arduino-pico releases, here consider number of bytes
+  //while (audioOut.availableForWrite() > BUFFER_SIZE + 17  ) {
+  static int dummy = 0;
+  dummy++;
+  while (dummy >= BUFFER_SIZE ) {
+    dummy -= BUFFER_SIZE;
+    
+    // process buffer
+    dsp_tick = micros();
+
+    synthFMalt_OSCalt_process_bufferTo_simplest(contextv0, BUFFER_SIZE, raw_buff);
+
+    // convert obtuse buffer to output buffer
+    fix16_t out;
+    for (size_t i = 0; i < BUFFER_SIZE; i++) {
+      out = raw_buff[i];
+      // returned float should be between -1 and 1, enforced with saturator
+      // shortcut, instead of fixed_to_float * 32767, *almost* the same and vastly improve perf with buffered version
+      // Note: use a greater divider than 2 to scale-down values as a crude way to avoid saturation, instead of ad-hoc Saturator
+      buff[i] = out / 4 - ( out >> 16);
+    }
+
+    dsp_time += micros() - dsp_tick;
+
+    // do ouput
+    /*for (int i = 0; i < BUFFER_SIZE; i++) {
+      audioOut.write(buff[i]);
+      }*/
+  }
+
+  // debug
+  int newTick = micros();
+  if (newTick - tick >= 1000000) {
+    Serial.print("Running strong! DSP time (useconds): ");
+    Serial.print(dsp_time);
+    Serial.print(" ("); Serial.print((float)dsp_time / (newTick - tick)); Serial.println("% CPU)");
+    dsp_time = 0;
+    tick += 1000000;
+  }
+
+}
---
+++ make_vult_simple.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "Generate FM synth with on-the-fly wavetable"
+vultc -ccode vult/voice.vult -real fixed -i vult/synth_FMalt -i vult/buffer_medium -o src/synthFMalt -output-prefix synthFMalt_
+
+
+echo "Hack init"
+# check at best as we can that we will replace the correct pieces
+INIT_STRUCT_COUNT=`grep ".*_ctx_type_.* _ctx;" src/* | wc -l`
+INIT_SET_COUNT=`grep "_output_ = _ctx;" src/* | wc -l`
+
+if [ "$INIT_STRUCT_COUNT" = "$INIT_SET_COUNT" ] ; then
+    echo "Will replace $INIT_STRUCT_COUNT occurrences"
+    # he wave to go through each file to prevent sed to update file even without any match
+    # FIXME: actually useless because vult will next time override due to changes.
+    # So we could also check for `grep "void .*__ctx_type_.*_init(.*__ctx_type.* &_output_){" src/* | wc -l` ?
+    for FILE in src/*
+    do
+	INIT_NB=`grep ".*_ctx_type_.* _ctx;" $FILE | wc -l`;
+	if [ $((INIT_NB))  -gt 0 ] ; then
+	    sed -i "s/\(.*\)_ctx_type_\(.*\) _ctx;/\1_ctx_type_\2 \&_ctx = _output_;/" $FILE
+	    sed -i "s/_output_ = _ctx;//" $FILE
+	fi
+    done
+else
+    echo "ABORT: mismatch between struct ($INIT_STRUCT_COUNT) and affectation ($INIT_SET_COUNT) count."
+fi
